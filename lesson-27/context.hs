418ef1433dd3dd0e0acb90ff51c2469552a63727
Shia <rise.shia@gmail.com>
2017-11-26 10:01:42 +0900

    Lesson 27

---
+++ lesson-27/context.hs
@@ -0,0 +1,13 @@
+data User = User
+   { name :: String
+   , gamerId :: Int
+   , score :: Int } deriving Show
+
+serverUsername :: Maybe String
+serverUsername = Just "Sue"
+
+serverGamerId :: Maybe Int
+serverGamerId = Just 1337
+
+serverScore :: Maybe Int
+serverScore = Just 9001
